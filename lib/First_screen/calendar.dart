d596b5cb72b784e1ae061ce81c53e1aab54c4b6f
unknown <gurals9368@gmail.com>
2021-02-04 16:01:58 +0900

    Second Check

--- lib/First_screen/calendar.dart
+++ lib/First_screen/calendar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-import '';
 // Example holidays
 final Map<DateTime, List> _holidays = {
   DateTime(2020, 1, 1): ['New Year\'s Day'],
@@ -34,53 +33,53 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       _selectedDay.subtract(Duration(days: 30)): [
         'Event A0',
         'Event B0',
-        'Event C0'
+        'Event C0',
       ],
-      _selectedDay.subtract(Duration(days: 27)): ['Event A1'],
+      _selectedDay.subtract(Duration(days: 27)): ['Event A1',],
       _selectedDay.subtract(Duration(days: 20)): [
         'Event A2',
         'Event B2',
         'Event C2',
-        'Event D2'
+        'Event D2',
       ],
-      _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3'],
+      _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3',],
       _selectedDay.subtract(Duration(days: 10)): [
         'Event A4',
         'Event B4',
-        'Event C4'
+        'Event C4',
       ],
       _selectedDay.subtract(Duration(days: 4)): [
         'Event A5',
         'Event B5',
-        'Event C5'
+        'Event C5',
       ],
-      _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
-      _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
+      _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6',],
+      _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7',],
       _selectedDay.add(Duration(days: 1)): [
         'Event A8',
         'Event B8',
         'Event C8',
-        'Event D8'
+        'Event D8',
       ],
       _selectedDay.add(Duration(days: 3)):
       Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
       _selectedDay.add(Duration(days: 7)): [
         '동계 동아리활동 13:00',
         'Coin 동아리 어플 개발회의 13:00',
-        '동아리방 대청소 16:00'
+        '동아리방 대청소 16:00',
       ],
       _selectedDay.add(Duration(days: 11)): ['Event A11', 'Event B11'],
       _selectedDay.add(Duration(days: 17)): [
         'Event A12',
         'Event B12',
         'Event C12',
-        'Event D12'
+        'Event D12',
       ],
       _selectedDay.add(Duration(days: 22)): ['Event A13', 'Event B13'],
       _selectedDay.add(Duration(days: 26)): [
         'Event A14',
         'Event B14',
-        'Event C14'
+        'Event C14',
       ],
     };
 
@@ -139,6 +138,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       ),
     );
   }
+
 
   // Simple TableCalendar configuration (using Styles)
   Widget _buildTableCalendar() {
