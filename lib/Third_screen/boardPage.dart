01bcdd0ad163189a59a1990e71f73b78f19152ba
parkjeonghyeok <qkrwjdgur12@naver.com>
2021-02-08 17:53:56 +0900

    third merge-3

--- lib/Third_screen/boardPage.dart
+++ lib/Third_screen/boardPage.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:coin_main/freeBoard.dart';
 
-
 class boardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
