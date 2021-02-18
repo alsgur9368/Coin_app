139991203d158ff8a21752f45fb3d470382460d6
sungho <gyogook608@naver.com>
2021-02-18 16:34:04 +0900

    second_screen responsible

--- lib/productAdd.dart
+++ lib/productAdd.dart
@@ -7,6 +7,14 @@ class ProductAdd extends StatefulWidget {
 }
 
 class _ProductAddState extends State<ProductAdd> {
+  double height(double value) {
+    return MediaQuery.of(context).size.height * (value / 812);
+  }
+
+  double width(double value) {
+    return MediaQuery.of(context).size.width * (value / 375);
+  }
+
   TextEditingController productNameController = TextEditingController();
   TextEditingController priceController = TextEditingController();
   TextEditingController productExplainController = TextEditingController();
@@ -19,7 +27,7 @@ class _ProductAddState extends State<ProductAdd> {
           return Container(
             height: MediaQuery.of(context).size.height * 0.4,
             child: Center(
-              child: Text("Welcome to AndroidVille!"),
+              child: Text("완료"),
             ),
           );
         });
@@ -29,7 +37,7 @@ class _ProductAddState extends State<ProductAdd> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 70,
+        toolbarHeight: height(70),
         elevation: 0,
         backgroundColor: Color(0xfffcfcfc),
         leading: IconButton(
@@ -51,87 +59,98 @@ class _ProductAddState extends State<ProductAdd> {
         bottom: PreferredSize(
           child: Container(
             color: Colors.grey[300],
-            height: 2,
+            height: height(2),
           ),
           preferredSize: Size.fromHeight(2),
         ),
       ),
       body: Column(
         children: [
-          Padding(
-            padding:
-                const EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
-            child: TextField(
-                controller: productNameController,
-                decoration: InputDecoration(
-                    hintText: ' 비품명',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(6)))),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8, right: 7),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  width: 180,
-                  height: 48,
-                  child: OutlineDropdownButton(
-                      isExpanded: true,
-                      isDense: false,
-                      hint: Text('카테고리'),
-                      value: _selectedText,
-                      items: <String>['book', 'tool', 'cable', 'mornitor']
-                          .map((value) {
-                        return new DropdownMenuItem<String>(
-                            value: value, child: Text(value));
-                      }).toList(),
-                      onChanged: (val) {
-                        setState(() {
-                          _selectedText = val;
-                        });
-                      }),
-                ),
-                Container(
-                  width: 180,
-                  height: 48,
-                  child: TextField(
-                      keyboardType: TextInputType.number,
-                      controller: priceController,
-                      decoration: InputDecoration(
-                          suffixText: '원',
-                          hintText: ' 가격',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(6)))),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-            child: Container(
-              height: 250,
-              width: 370,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(6),
-                border: Border.all(),
-              ),
-              child: TextFormField(
-                cursorColor: Colors.black,
-                minLines: 1,
-                maxLines: 12,
-                controller: productExplainController,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  filled: true,
-                  fillColor: Colors.transparent,
-                  hintText: '글을 작성해주세요',
-                ),
-              ),
-            ),
-          ),
+          productName_(),
+          productCategory_(),
+          productIntroduce_(),
         ],
       ),
     );
   }
+
+  Widget productName_(){
+    return  Padding(
+      padding: EdgeInsets.fromLTRB(width(20),height(10),width(20),height(10)),
+      child: TextField(
+          controller: productNameController,
+          decoration: InputDecoration(
+              hintText: ' 비품명',
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6)))),
+    );
+  }
+
+  Widget productCategory_(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          padding: EdgeInsets.fromLTRB(width(6),0,0,0),
+          width: width(167),
+          height: height(48),
+          child: OutlineDropdownButton(
+              isExpanded: true,
+              isDense: false,
+              hint: Text('카테고리'),
+              value: _selectedText,
+              items: <String>['book', 'tool', 'cable', 'mornitor']
+                  .map((value) {
+                return new DropdownMenuItem<String>(
+                    value: value, child: Text(value));
+              }).toList(),
+              onChanged: (val) {
+                setState(() {
+                  _selectedText = val;
+                });
+              }),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(0,0,width(6),0),
+          width: width(167),
+          height: height(48),
+          child: TextField(
+              keyboardType: TextInputType.number,
+              controller: priceController,
+              decoration: InputDecoration(
+                  suffixText: '원',
+                  hintText: ' 가격',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(6)))),
+        ),
+      ],
+    );
+  }
+
+  Widget productIntroduce_(){
+    return Padding(
+      padding: EdgeInsets.fromLTRB(width(20),height(10),width(20),0),
+      child: Container(
+        height: height(250),
+        width: width(370),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          border: Border.all(),
+        ),
+        child: TextFormField(
+          cursorColor: Colors.black,
+          minLines: 1,
+          maxLines: 12,
+          controller: productExplainController,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            filled: true,
+            fillColor: Colors.transparent,
+            hintText: '글을 작성해주세요',
+          ),
+        ),
+      ),
+    );
+  }
+
 }
