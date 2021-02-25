3fdfd62e2fc08535c4800d2e5c191608b22783e4
dohyeon <gold6219@naver.com>
2021-02-25 15:46:08 +0900

    boardpage

--- lib/qna.dart
+++ lib/qna.dart
@@ -44,6 +44,14 @@ class _QnAState extends State<QnA> {
   String filter;
   TextEditingController searchController = TextEditingController();
 
+  double height(double value) {
+    return MediaQuery.of(context).size.height * (value / 812);
+  }
+
+  double width(double value) {
+    return MediaQuery.of(context).size.width * (value / 375);
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -53,14 +61,6 @@ class _QnAState extends State<QnA> {
         filter = searchController.text;
       });
     });
-  }
-
-  double height(double value) {
-    return MediaQuery.of(context).size.height * (value / 812);
-  }
-
-  double width(double value) {
-    return MediaQuery.of(context).size.width * (value / 375);
   }
 
   @override
@@ -79,16 +79,16 @@ class _QnAState extends State<QnA> {
         elevation: 0,
         backgroundColor: Color(0xfffcfcfc),
         leading: IconButton(
-            icon: Icon(Icons.chevron_left, color: Colors.black,size: width(28),),
+            icon: Icon(Icons.chevron_left, color: Colors.black,size: width(28)),
             onPressed: () {
               Navigator.pop(context);
             }),
         centerTitle: true,
-        title: Text('Q&A', textAlign: TextAlign.center, style: TextStyle(color: Colors.black,fontSize: width(16))),
+        title: Text('QnA', textAlign: TextAlign.center, style: TextStyle(color: Colors.black,fontSize: width(16))),
         actions: <Widget>[
           IconButton(
               padding: EdgeInsets.only(right: width(10)),
-              icon: Icon(Icons.add_sharp, color: Colors.black,size: width(28)),
+              icon: Icon(Icons.add_sharp, color: Colors.black,size: width(28),),
               onPressed: () {
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => createPost()));
@@ -105,95 +105,105 @@ class _QnAState extends State<QnA> {
       ),
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: TextField(
-              controller: searchController,
-              decoration: InputDecoration(
-                fillColor: Colors.grey[200],
-                filled: true,
-                hintText: '검색어를 입력하세요.',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-                itemCount: _list.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return filter == null || filter == ""
-                      ? Column(
-                    children: <Widget>[
-                      ListTile(
-                        title: Text('${_list[index].titles}'),
-                        subtitle: Text('${_list[index].names}'),
-                        onTap: () => _onTapItem(context, _list[index]),
-                        trailing: FittedBox(
-                          child: Row(
-                            children: <Widget>[
-                              Container(
-                                child: Row(
-                                  children: [
-                                    SvgPicture.asset(
-                                        "images/coin_source/icon_list_bookmark_12px.svg"),
-                                    SizedBox(width: 4),
-                                    Text('0'),
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.only(left: 6),
-                                child: Row(
-                                  children: [
-                                    SvgPicture.asset(
-                                        "images/coin_source/icon_list_comment_12px.svg"),
-                                    SizedBox(width: 4),
-                                    Text('3'),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Divider(
-                        thickness: 0.5,
-                      ),
-                    ],
-                  )
-                      : '${_list[index].titles}'
-                      .toLowerCase()
-                      .contains(filter.toLowerCase())
-                      ? Column(
-                    children: <Widget>[
-                      ListTile(
-                        title: Text('${_list[index].titles}'),
-                        subtitle: Text('${_list[index].names}'),
-                        onTap: () =>
-                            _onTapItem(context, _list[index]),
-                      ),
-                      Divider(
-                        thickness: 0.5,
-                      )
-                    ],
-                  )
-                      : Container();
-                }),
-          ),
+          searchBox_(),
+          freeList_(),
         ],
       ),
     );
   }
 
+  Widget searchBox_() {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(width(20), height(20), width(20), height(20)),
+      child: TextField(
+        controller: searchController,
+        decoration: InputDecoration(
+          fillColor: Colors.grey[200],
+          filled: true,
+          hintText: '검색어를 입력하세요.',
+          prefixIcon: Icon(Icons.search),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          contentPadding: EdgeInsets.fromLTRB(width(20), height(15), width(20), height(15)),
+        ),
+      ),
+    );
+  }
+
+  Widget freeList_() {
+    return Expanded(
+      child: ListView.builder(
+          itemCount: _list.length,
+          itemBuilder: (BuildContext context, int index) {
+            return filter == null || filter == ""
+                ? Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text('${_list[index].titles}'),
+                  subtitle: Text('${_list[index].names}'),
+                  onTap: () => _onTapItem(context, _list[index]),
+                  contentPadding: EdgeInsets.only(left: width(25)),
+                  trailing: FittedBox(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          child: Row(
+                            children: [
+                              SvgPicture.asset(
+                                  "images/coin_source/icon_list_bookmark_12px.svg"),
+                              SizedBox(width: width(4)),
+                              Text('0'),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: width(6),right: width(25)),
+                          child: Row(
+                            children: [
+                              SvgPicture.asset(
+                                  "images/coin_source/icon_list_comment_12px.svg"),
+                              SizedBox(width: width(4)),
+                              Text('3'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(
+                  thickness: 0.8,
+                  endIndent: width(20),
+                  indent: width(20),
+                ),
+              ],
+            )
+                : '${_list[index].titles}'
+                .toLowerCase()
+                .contains(filter.toLowerCase())
+                ? Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text('${_list[index].titles}'),
+                  subtitle: Text('${_list[index].names}'),
+                  onTap: () =>
+                      _onTapItem(context, _list[index]),
+                ),
+                Divider(
+                  thickness: 0.8,
+                )
+              ],
+            )
+                : Container();
+          }),
+    );
+  }
   void _onTapItem(BuildContext context, BoardList boardList) {
     Navigator.push(context, MaterialPageRoute(builder: (context) => Post()));
   }
